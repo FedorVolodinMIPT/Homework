eb37e1dfffa09a037865057f7bdfbc0afdba4e9b
FedorVolodinMIPT <volodin.fa@phystech.edu>
2024-12-18 22:43:12 +0300

    Update №6(03.10.24).homework.py

--- №6(03.10.24).homework.py
+++ №6(03.10.24).homework.py
@@ -4,7 +4,7 @@ def MNK(x, y):
     x = np.array(x)
     y = np.array(y)
 
-    k = ((x * y).mean() - x.mean() * y.mean()) / (x*2).mean() - (x.mean())*2
+    k = ((x * y).mean() - x.mean() * y.mean()) / (x**2).mean() - (x.mean())**2
     b = y.mean() - k * x.mean()
     print(k, b)
 
