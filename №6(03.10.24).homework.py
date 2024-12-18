ef6a6d8ae1ce524a50f89590feced7316e5f51e2
FedorVolodinMIPT <volodin.fa@phystech.edu>
2024-12-18 22:19:09 +0300

    Create №6(03.10.24).homework.py

---
+++ №6(03.10.24).homework.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def MNK(x, y):
+    x = np.array(x)
+    y = np.array(y)
+
+    k = ((x * y).mean() - x.mean() * y.mean()) / (x*2).mean() - (x.mean())*2
+    b = y.mean() - k * x.mean()
+    print(k, b)
+
+x = list(map(int, input("Введите значения x: ").split()))
+y = list(map(int, input("Введите значения y: ").split()))
+
+MNK(x, y)
